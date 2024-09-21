817d4e8cbb167055aab52f82a420cf47e4c2ef93
andre.ramos <andre.ramos@witheleos.com>
2024-09-21 18:52:45 +0100

    Create project

---
+++ src/main/java/com/example/PhoneContactApp/PhoneContactAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhoneContactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneContactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneContactAppApplication.class, args);
+	}
+
+}
